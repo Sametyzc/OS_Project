0b87d00254e42ef3f48933822947b7feb2b65af6
Samet Yazıcı <sametyzc@hotmail.com>
2019-11-26 16:12:24 +0300

    Pipe fonksiyonu
    2 parametre ile çalışacak şekilde pipe fonksiyonu yapıldı

--- Program.c
+++ Program.c
@@ -1,11 +1,90 @@
 #include <stdio.h>
-
 #include <string.h>
-
 #include <unistd.h>
-
 #include <limits.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
+
+/* Pipe denemek için örnek 2 kod
+  char* komutlar[]={"ls","less"};
+  char* girdiler[]={"-1",NULL};
+  if(Pipe(komutlar,girdiler)<0)
+  {
+    printf("pipe olusurken hata\n");
+  }
+  return 0;
+
+*/
+//Pipe için girdi ve çıktı uçlarını belirttik
+#define READ_END 0
+#define WRITE_END 1
+//Commands komutların bulunduğu bir dizi
+//Arguments commands ta sıfırıncı indikste hangi komut var ise arguments[0] da da onun parametresi olmalı
+int Pipe(char* commands[],char* arguments[]){
+  //Hiç komut girilmediyse hata veriyor
+  if(commands[0]==NULL)
+  {
+    printf("Girilen komutlar yeterli degil!");
+    return -1;
+  }
+  //Fork yapmak için child id si
+  pid_t child_pid,child_pid2;
+  //pipe in girdi ve çıktı ucunu belirten dizi
+  int fd[2];
+  // dizimiz üzerinde pipe oluşturuldu
+  pipe(fd);
+  //Cocuk olusturmak için fork yapıldı
+  child_pid = fork();
+  //execvp fonksiyonuna parametre vermek için listeler
+  char* values1[] = {commands[0],arguments[0],NULL};
+  char* values2[] = {commands[1],arguments[1],NULL};
+  if(child_pid<0)
+  {
+    perror("Cocuk olusurken hata!");
+    return -1;
+  }
+  else if(child_pid==0)
+  {
+    //dup2 fonksiyonu kullanılarak printf fonksiyonlarının çıktısı pipe yazıldı;
+    //dup2 yaptıktan sonra tüm printfler ilk parametresine verilen yere yazılır burada pipe in girdi ucuna yazdırıldı
+    dup2(fd[WRITE_END], STDOUT_FILENO);
+    //Bunları niye kapatıyoruz bilmiyorum
+    close(fd[READ_END]);
+    close(fd[WRITE_END]);
+    //execvp fonksiyonuna hangi komutun çalışacağı ve parame
+    if(execvp(commands[0],values1)<0){
+      perror(commands[0]);
+      return -1;
+    }
+  }
+  else
+  {
+    child_pid2=fork();
+    if(child_pid2<0)
+    {
+      perror("Cocuk olusurken hata!");
+      return -1;
+    }
+    else if(child_pid2==0)
+    {
+      dup2(fd[READ_END], STDIN_FILENO);
+      close(fd[WRITE_END]);
+      close(fd[READ_END]);
+      if(execvp(commands[1],values2)<0){
+        perror(commands[1]);
+        return -1;
+      }
+    }
+    else{
+      wait(&child_pid2);
+    }
+    wait(&child_pid);
+  }
+  return 1;
+}
 int Recognizer(char * input) {
   //printf("%s",input);
   char * pch;
